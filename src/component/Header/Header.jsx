2cf8d6adb7c64ac8b3123d2f92811c3cda40fe53
kaziFahmid <nirjhorislam42@gmail.com>
2023-07-17 14:07:03 +0600

    some npm pacakge added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,7 +4,17 @@ export default function Header() {
   return (
     <div className="navbar bg-white  text-primary-content gap-8 flex justify-between items-center py-7">
     <a className="btn btn-ghost normal-case text-xl text-black">House<b className='text-red-500'>Hunting</b></a>
-    <ul className='flex justify-center items-center gap-5'>
+
+    
+
+
+
+
+
+
+
+
+    <ul className=' hidden md:flex justify-center items-center gap-5'>
         <li className='btn btn-ghost normal-case text-xl text-black hover:text-red-500'>Home+</li>
         <li  className='btn btn-ghost normal-case text-xl text-black hover:text-red-500'>About</li>
         <li  className='btn btn-ghost normal-case text-xl text-black hover:text-red-500'>Dashboard</li>
