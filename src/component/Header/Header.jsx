092a7e80f521b6afcee66251e4fa8d8de11e7730
kaziFahmid <nirjhorislam42@gmail.com>
2023-07-17 14:10:18 +0600

    responsive menu nav added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";                                       
+         
 export default function Header() {
+    const [visible, setVisible] = useState(false);
   return (
     <div className="navbar bg-white  text-primary-content gap-8 flex justify-between items-center py-7">
     <a className="btn btn-ghost normal-case text-xl text-black">House<b className='text-red-500'>Hunting</b></a>
 
-    
+    <div className="card flex md:hidden justify-content-center">
+    <Sidebar visible={visible} onHide={() => setVisible(false)}>
+        <h2>Sidebar</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+    </Sidebar>
+    <Button icon="pi pi-arrow-right" onClick={() => setVisible(true)} />
+</div>
+         
 
 
 
